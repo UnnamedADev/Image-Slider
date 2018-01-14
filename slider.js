9e79701682e2747ea3eb29ebe13fd44fa0b57966
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-14 15:45:10 +0100

    v0.0.4
    STRCUTURE
    + Added more slider images for example
    HTML
    + Added new images tags
    JAVASCRIPT
    + Created function that can build visual counter elements
    + Created function that can manage and refresh visual counter
    CSS
    + Modified progress bar background
    + Added style for visual counter holder and his sub-elements

--- slider.js
+++ slider.js
@@ -3,15 +3,15 @@
 $(document).ready(function(){
     
     //var
-    const refreshDuration = 7000;
-    const effectDuration = 1000;
+    const refreshDuration = 3000;
+    const effectDuration = 400;
     
     var elementsNumber;
     var sliderImages = [];
     
     //main
     getImages();
-    console.log(elementsNumber);
+    buildCounter();
     
     var pointer = 0;
     setTimeout(function(){
@@ -22,13 +22,16 @@ $(document).ready(function(){
     function refreshSlider(pointer){
         
         moveBar();
+        
+        
         if(pointer==0){
+
             $(sliderImages[elementsNumber-1]).fadeOut(effectDuration, function(){
                 $(sliderImages[pointer]).fadeIn(effectDuration);
                 pointer++;
             });
         }
-        
+
         $(sliderImages[pointer-1]).fadeOut(effectDuration, function(){
              $(sliderImages[pointer]).fadeIn(effectDuration);
             pointer++;
@@ -37,7 +40,7 @@ $(document).ready(function(){
         if(pointer==elementsNumber){
             pointer=0;
         }
-        
+        refreshCounter(pointer);
         setTimeout(function(){
             refreshSlider(pointer);
         },refreshDuration);
@@ -60,5 +63,26 @@ $(document).ready(function(){
             width: "0%"
         },refreshDuration, "linear");
     }
-
+    function buildCounter(){
+        
+        var validWidth = 100/elementsNumber+"%";
+        
+        var counterHolder = document.getElementsByClassName("counter")[0];
+        
+        for(var i=0;i<elementsNumber;i++){
+            var newOne = document.createElement("div");
+            newOne.style.width = validWidth;
+            counterHolder.appendChild(newOne);
+        }
+    }
+    function refreshCounter(pointer){
+        
+        var counterHolder = document.getElementsByClassName("counter")[0];
+        var counter = counterHolder.getElementsByTagName("div");
+        
+        for(var i=0;i<counter.length;i++){
+            counter[i].classList.remove("activeCounter");
+        }
+        counter[pointer].classList.add("activeCounter");
+    }
 });
